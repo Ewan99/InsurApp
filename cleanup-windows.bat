893796e42099b696cee4f4e52921591bc1ce9108
Ewan Duff <ewan4d@gmail.com>
2022-01-17 17:31:43 +0000

    Update cleanup-windows.bat

--- cleanup-windows.bat
+++ cleanup-windows.bat
@@ -1,6 +1,6 @@
-#removes all CA content + containers for restarting the application
-sudo rm -r ca
-sudo rm -r tlsca
+:: removes all CA content + containers for restarting the application
+rm -r ca
+rm -r tlsca
 docker rm -f insurapp_ca.insurapp.com_1
 docker rm -f insurapp_tlsca.insurapp.com_1
 docker rm -f InsurApp
