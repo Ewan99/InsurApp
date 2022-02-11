61d249dac72ef227d017e22d1cf3248eb032a05d
Ewan Duff <ewan4d@gmail.com>
2022-02-11 02:48:53 +0000

    added binaries + MSP progress

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,67 @@
 sudo docker build -t docker-insurapp .
+echo "#####################################"
+echo "#### Configuring Web Application ####"
+echo "#####################################"
+echo
 sudo docker run --name InsurApp-Web --network=host -p 3080:3080 -d docker-insurapp
+echo "Web App Server enabled!"
+echo
+echo "###########################################"
+echo "#### Configuring Certificate Authority ####"
+echo "###########################################"
+echo
+echo "Starting CA Containers..."
 sudo docker-compose -f docker-compose-ca.yaml up -d
+echo "CA Containers up!"
+echo
+echo "#################################################"
+echo "#### Copying necessary files to CA container ####"
+echo "#################################################"
+echo
+echo "Copying 'generatemsp.sh'..."
 docker cp ./generatemsp.sh insurapp_ca.insurapp.com_1:/
+echo "Copying 'generatemsp-tls.sh'..."
+docker cp ./generatemsp-tls.sh insurapp_tlsca.insurapp.com_1:/
+echo "Copying 'configtx.yaml'..."
 docker cp ./configtx.yaml insurapp_ca.insurapp.com_1:/
+echo "Copying 'configtxgen'..."
+docker cp ./configtxgen insurapp_ca.insurapp.com_1:/
+echo "Copying 'cryptogen'..."
+docker cp ./cryptogen insurapp_ca.insurapp.com_1:/
+echo "Copying 'ca' folder..."
 docker cp ./ca insurapp_ca.insurapp.com_1:/
-ddocker cp ./tlsca
+echo "Copying 'tlsca' folder..."
+docker cp ./tlsca insurapp_tlsca.insurapp.com_1:/
+echo "Copying 'hyperledger' folder..."
+docker cp ./hyperledger insurapp_ca.insurapp.com_1:/
+echo "Copying 'peers' folder..."
+docker cp ./peers insurapp_ca.insurapp.com_1:/
+echo "Copying 'orderers' folder..."
+docker cp ./orderers insurapp_ca.insurapp.com_1:/
+echo
+echo "All files copied!"
+echo
+echo "##########################"
+echo "#### Configuring MSPs ####"
+echo "##########################"
+echo
+echo "Configuring CA MSP..."
+echo
 sudo docker exec -it insurapp_ca.insurapp.com_1 sh ./generatemsp.sh
-sudo docker exec -it insurapp_ca.insurapp.com_1 sh ./generatemsp-tls.sh
-sudo docker exec -it insurapp_ca.insurapp.com_1 sh ./configtx.yaml
-sudo docker exec -it insurapp_ca.insurapp.com_1 sh mkdir admincerts
+echo
+echo "Configuring TLS CA MSP..."
+echo
+sudo docker exec -it insurapp_tlsca.insurapp.com_1 sh ./generatemsp-tls.sh
+echo
+echo "Building Channels..."
+echo
+sudo docker exec -it insurapp_ca.insurapp.com_1 bash -c "mkdir admincerts"
+sudo docker exec -it insurapp_ca.insurapp.com_1 bash -c "mkdir channel-artifacts"
+echo
+echo "Creating Genesis Blocks..."
+echo
+sudo docker exec -it insurapp_ca.insurapp.com_1 bash -c "./configtxgen -profile genesis -outputBlock channel-artifacts/genesis.block -channelID default"
+echo
+echo "Create Channels..."
+echo
+sudo docker exec -it insurapp_ca.insurapp.com_1 bash -c "./configtxgen -profile default -outputCreateChannelTx channel-artifacts/default.tx -channelID default"
--- generatemsp-tls.sh
+++ generatemsp-tls.sh
@@ -6,7 +6,7 @@ fabric-ca-client register --id.name orderer1.insurance.com --id.secret secret --
 ##
 fabric-ca-client register --id.name peer1.insurance.com --id.secret secret --id.type peer -u http://localhost:8054
 ##
-fabric-ca-client register --id.name admin.insurance.com --id.secret secret --id.type admin -u http://localhost:8054
+fabric-ca-client register --id.name admin1.insurance.com --id.secret secret --id.type admin -u http://localhost:8054
 ##
 ##
 ##
@@ -14,24 +14,24 @@ fabric-ca-client register --id.name orderer1.garage.com --id.secret secret --id.
 ##
 fabric-ca-client register --id.name peer1.garage.com --id.secret secret --id.type peer -u http://localhost:8054
 ##
-fabric-ca-client register --id.name admin.garage.com --id.secret secret --id.type admin -u http://localhost:8054
+fabric-ca-client register --id.name admin1.garage.com --id.secret secret --id.type admin -u http://localhost:8054
 ##
 ##
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "orderer1.insurance.com" -u http://orderer1.insurance.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/insurance/orderer1/
+fabric-ca-client enroll --csr.hosts "orderer1.insurance.com" -u http://orderer1.insurance.com:secret@localhost:8054 -M tls
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "peer1.insurance.com" -u http://peer1.insurance.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/insurance/peer1/
+fabric-ca-client enroll --csr.hosts "peer1.insurance.com" -u http://peer1.insurance.com:secret@localhost:8054 -M tls
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "admin1.insurance.com" -u http://admin1.insurance.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/insurance/admin1/
+fabric-ca-client enroll --csr.hosts "admin1.insurance.com" -u http://admin1.insurance.com:secret@localhost:8054 -M tls
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "orderer1.garage.com" -u http://orderer1.garage.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/garage/orderer1/
+fabric-ca-client enroll --csr.hosts "orderer1.garage.com" -u http://orderer1.garage.com:secret@localhost:8054 -M tls
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "peer1.garage.com" -u http://peer1.garage.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/garage/peer1/
+fabric-ca-client enroll --csr.hosts "peer1.garage.com" -u http://peer1.garage.com:secret@localhost:8054 -M tls
 ##
-export FABRIC_CA_HOME=./orderers/insurers/orderer1/
-fabric-ca-client enroll --csr.hosts "admin1.garage.com" -u http://admin1.garage.com:secret@localhost:8054
+export FABRIC_CA_HOME=./orderers/garage/admin1/
+fabric-ca-client enroll --csr.hosts "admin1.garage.com" -u http://admin1.garage.com:secret@localhost:8054 -M tls
