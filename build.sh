934fddf61472cebaa9011e568cc1eb149cba58d5
Ewan Duff <ewan4d@gmail.com>
2022-04-05 18:54:36 +0100

    April-UpToDate

--- build.sh
+++ build.sh
@@ -11,21 +11,143 @@ echo "################################"
 echo
 echo "Starting CA Containers..."
 sudo docker-compose -f docker-compose-ca.yaml up -d
-echo "CA Containers up!"
+echo "CA Containers up! Waiting 20s for containers to initialize..."
+sleep 20
+echo
+echo "#########################"
+echo "#### Configuring CAs ####"
+echo "#########################"
+echo
+###peers
+export FABRIC_CA_HOME=./rootca
+fabric-ca-client enroll -u http://admin:adminpw@localhost:7054
+
+fabric-ca-client register --id.name peer1.iapeer.com --id.secret secret --id.type peer -u http://localhost:7054
+fabric-ca-client register --id.name peer2.iapeer.com --id.secret secret --id.type peer -u http://localhost:7054
+fabric-ca-client register --id.name peer3.iapeer.com --id.secret secret --id.type peer -u http://localhost:7054
+
+fabric-ca-client register --id.name admin1.iapeer.com --id.secret adminpw --id.type admin -u http://localhost:7054
+fabric-ca-client register --id.name admin2.iapeer.com --id.secret adminpw --id.type admin -u http://localhost:7054
+fabric-ca-client register --id.name admin3.iapeer.com --id.secret adminpw --id.type admin -u http://localhost:7054
+
+export FABRIC_CA_HOME=./peers/iapeer/peer1/
+fabric-ca-client enroll --csr.hosts "peer1.iapeer.com" -u http://peer1.iapeer.com:secret@localhost:7054
+export FABRIC_CA_HOME=./peers/iapeer/peer2/
+fabric-ca-client enroll --csr.hosts "peer2.iapeer.com" -u http://peer2.iapeer.com:secret@localhost:7054
+export FABRIC_CA_HOME=./peers/iapeer/peer3/
+fabric-ca-client enroll --csr.hosts "peer3.iapeer.com" -u http://peer3.iapeer.com:secret@localhost:7054
+
+export FABRIC_CA_HOME=./peers/iapeer/peer1/msp/user/
+fabric-ca-client enroll --csr.hosts "admin1.iapeer.com" -u http://admin1.iapeer.com:adminpw@localhost:7054 -M admin
+export FABRIC_CA_HOME=./peers/iapeer/peer2/msp/user/
+fabric-ca-client enroll --csr.hosts "admin2.iapeer.com" -u http://admin2.iapeer.com:adminpw@localhost:7054 -M admin
+export FABRIC_CA_HOME=./peers/iapeer/peer3/msp/user/
+fabric-ca-client enroll --csr.hosts "admin3.iapeer.com" -u http://admin3.iapeer.com:adminpw@localhost:7054 -M admin
+###orderers###
+export FABRIC_CA_HOME=./rootca
+fabric-ca-client enroll -u http://admin:adminpw@localhost:7055
+
+fabric-ca-client register --id.name orderer1.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:7055
+fabric-ca-client register --id.name orderer2.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:7055
+fabric-ca-client register --id.name orderer3.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:7055
+
+fabric-ca-client register --id.name admin1.iaorderer.com --id.secret adminpw --id.type admin -u http://localhost:7055
+fabric-ca-client register --id.name admin2.iaorderer.com --id.secret adminpw --id.type admin -u http://localhost:7055
+fabric-ca-client register --id.name admin3.iaorderer.com --id.secret adminpw --id.type admin -u http://localhost:7055
+
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer1/
+fabric-ca-client enroll --csr.hosts "orderer1.iaorderer.com" -u http://orderer1.iaorderer.com:secret@localhost:7055
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer2/
+fabric-ca-client enroll --csr.hosts "orderer2.iaorderer.com" -u http://orderer2.iaorderer.com:secret@localhost:7055
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer3/
+fabric-ca-client enroll --csr.hosts "orderer3.iaorderer.com" -u http://orderer3.iaorderer.com:secret@localhost:7055
+
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer1/msp/user/
+fabric-ca-client enroll --csr.hosts "admin1.iaorderer.com" -u http://admin1.iaorderer.com:adminpw@localhost:7055 -M admin
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer2/msp/user/
+fabric-ca-client enroll --csr.hosts "admin2.iaorderer.com" -u http://admin2.iaorderer.com:adminpw@localhost:7055 -M admin
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer3/msp/user/
+fabric-ca-client enroll --csr.hosts "admin3.iaorderer.com" -u http://admin3.iaorderer.com:adminpw@localhost:7055 -M admin
+
+
+echo
+echo "#############################"
+echo "#### Configuring TLS CAs ####"
+echo "#############################"
+echo
+###peers###
+export FABRIC_CA_HOME=./roottlsca
+fabric-ca-client enroll -u http://admin:adminpw@localhost:8054
+
+fabric-ca-client register --id.name peer1.iapeer.com --id.secret secret --id.type peer -u http://localhost:8054
+fabric-ca-client register --id.name peer2.iapeer.com --id.secret secret --id.type peer -u http://localhost:8054
+fabric-ca-client register --id.name peer3.iapeer.com --id.secret secret --id.type peer -u http://localhost:8054
+
+export FABRIC_CA_HOME=./peers/iapeer/peer1/
+fabric-ca-client enroll --csr.hosts "peer1.iapeer.com" -u http://peer1.iapeer.com:secret@localhost:8054 -M tls
+export FABRIC_CA_HOME=./peers/iapeer/peer2/
+fabric-ca-client enroll --csr.hosts "peer2.iapeer.com" -u http://peer2.iapeer.com:secret@localhost:8054 -M tls
+export FABRIC_CA_HOME=./peers/iapeer/peer3/
+fabric-ca-client enroll --csr.hosts "peer3.iapeer.com" -u http://peer3.iapeer.com:secret@localhost:8054 -M tls
+###orderers###
+export FABRIC_CA_HOME=./roottlsca
+fabric-ca-client enroll -u http://admin:adminpw@localhost:8055
+
+fabric-ca-client register --id.name orderer1.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:8055
+fabric-ca-client register --id.name orderer2.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:8055
+fabric-ca-client register --id.name orderer3.iaorderer.com --id.secret secret --id.type orderer -u http://localhost:8055
+
+
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer1/
+fabric-ca-client enroll --csr.hosts "orderer1.iaorderer.com" -u http://orderer1.iaorderer.com:secret@localhost:8055 -M tls
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer2/
+fabric-ca-client enroll --csr.hosts "orderer2.iaorderer.com" -u http://orderer2.iaorderer.com:secret@localhost:8055 -M tls
+export FABRIC_CA_HOME=./orderers/iaorderer/orderer3/
+fabric-ca-client enroll --csr.hosts "orderer3.iaorderer.com" -u http://orderer3.iaorderer.com:secret@localhost:8055 -M tls
+
+
+sudo mv orderers/iaorderer/orderer1/tls/keystore/* orderers/iaorderer/orderer1/tls/keystore/server.key
+sudo mv orderers/iaorderer/orderer2/tls/keystore/* orderers/iaorderer/orderer2/tls/keystore/server.key
+sudo mv orderers/iaorderer/orderer3/tls/keystore/* orderers/iaorderer/orderer3/tls/keystore/server.key
+
+sudo mv peers/iapeer/peer1/tls/keystore/* peers/iapeer/peer1/tls/keystore/server.key
+sudo mv peers/iapeer/peer2/tls/keystore/* peers/iapeer/peer2/tls/keystore/server.key
+sudo mv peers/iapeer/peer3/tls/keystore/* peers/iapeer/peer3/tls/keystore/server.key
+
 echo
 echo "##########################"
 echo "#### Configuring MSPs ####"
 echo "##########################"
 echo
-sudo cp ca/server.crt orderers/iaorderer/msp/cacerts
-sudo cp ca/server.crt peers/iapeer/msp/cacerts
-sudo cp tlsca/server.crt orderers/iaorderer/msp/tlscacerts
-sudo cp tlsca/server.crt peers/iapeer/msp/tlscacerts
-sudo cp orderers/iaorderer/orderer1/msp/user/admin/signcerts/cert.pem orderers/iaorderer/msp/admincerts/
-sudo cp peers/iapeer/peer1/msp/user/admin/signcerts/cert.pem peers/iapeer/msp/admincerts/
+sudo cp ca/orderer-ca-server.crt orderers/iaorderer/msp/cacerts
+sudo cp ca/peer-ca-server.crt peers/iapeer/msp/cacerts
+sudo cp tlsca/orderer-tlsca-server.crt orderers/iaorderer/msp/tlscacerts
+sudo cp tlsca/peer-tlsca-server.crt peers/iapeer/msp/tlscacerts
+
+sudo cp orderers/iaorderer/orderer1/msp/user/admin/signcerts/cert.pem orderers/iaorderer/msp/admincerts/ordereradmin1.pem
+sudo cp orderers/iaorderer/orderer2/msp/user/admin/signcerts/cert.pem orderers/iaorderer/msp/admincerts/ordereradmin2.pem
+sudo cp orderers/iaorderer/orderer3/msp/user/admin/signcerts/cert.pem orderers/iaorderer/msp/admincerts/ordereradmin3.pem
+
+sudo cp peers/iapeer/peer1/msp/user/admin/signcerts/cert.pem peers/iapeer/msp/admincerts/peeradmin1.pem
+sudo cp peers/iapeer/peer2/msp/user/admin/signcerts/cert.pem peers/iapeer/msp/admincerts/peeradmin2.pem
+sudo cp peers/iapeer/peer3/msp/user/admin/signcerts/cert.pem peers/iapeer/msp/admincerts/peeradmin3.pem
+
 sudo cp orderers/iaorderer/orderer1/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer1/msp/admincerts
+sudo cp orderers/iaorderer/orderer1/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer1/msp/user/admin/admincerts
+sudo cp orderers/iaorderer/orderer2/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer2/msp/admincerts
+sudo cp orderers/iaorderer/orderer2/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer2/msp/user/admin/admincerts
+sudo cp orderers/iaorderer/orderer3/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer3/msp/admincerts
+sudo cp orderers/iaorderer/orderer3/msp/user/admin/signcerts/cert.pem orderers/iaorderer/orderer3/msp/user/admin/admincerts
+
 sudo cp peers/iapeer/peer1/msp/user/admin/signcerts/cert.pem peers/iapeer/peer1/msp/admincerts
 sudo cp peers/iapeer/peer1/msp/user/admin/signcerts/cert.pem peers/iapeer/peer1/msp/user/admin/admincerts/
+
+sudo cp peers/iapeer/peer2/msp/user/admin/signcerts/cert.pem peers/iapeer/peer2/msp/admincerts
+sudo cp peers/iapeer/peer2/msp/user/admin/signcerts/cert.pem peers/iapeer/peer2/msp/user/admin/admincerts/
+
+sudo cp peers/iapeer/peer3/msp/user/admin/signcerts/cert.pem peers/iapeer/peer3/msp/admincerts
+sudo cp peers/iapeer/peer3/msp/user/admin/signcerts/cert.pem peers/iapeer/peer3/msp/user/admin/admincerts/
+
 echo
 echo "####################################################"
 echo "#### Creating Genesis Block and Default Channel ####"
@@ -39,13 +161,15 @@ echo "#### Starting Blockchain Network ####"
 echo "#####################################"
 echo
 sudo docker-compose up -d
+echo "All peers created! - Waiting 10s for peers to initalize"
+sleep 30
 echo
 echo "############################################"
 echo "#### Creating and joining Peer Channels ####"
 echo "############################################"
 echo
-sudo docker exec -it cli bash -c peer channel create -f channel-artifacts/default.tx -c default -o orderer1.iaorderer.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/iapeer/msp/tlscacerts/server.crt
-sudo docker exec -it cli bash -c peer channel join -b default.block -o orderer1.iaorderer.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/iapeer/msp/tlscacerts/server.crt
+#sudo docker exec -it cli bash -c peer channel create -f channel-artifacts/default.tx -c default -o orderer1.iaorderer.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/iapeer/msp/tlscacerts/server.crt
+#sudo docker exec -it cli bash -c peer channel join -b default.block -o orderer1.iaorderer.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/iapeer/msp/tlscacerts/server.crt
 echo
 echo "###################"
 echo "#### Finished! ####"
--- destroy.sh
+++ destroy.sh
@@ -1,17 +1,24 @@
-sudo docker rm -f insurappremake_ca.insurapp.com_1
-sudo docker rm -f insurappremake_tlsca.insurapp.com_1
-sudo docker rm -f insurappremake_orderer1.iaorderer.com_1
+sudo docker rm -f ca.orderer.insurapp.com
+sudo docker rm -f tlsca.orderer.insurapp.com
+sudo docker rm -f ca.peer.insurapp.com
+sudo docker rm -f tlsca.peer.insurapp.com
+sudo docker rm -f orderer1.iaorderer.com
+sudo docker rm -f orderer2.iaorderer.com
+sudo docker rm -f orderer3.iaorderer.com
 sudo docker rm -f peer1.iapeer.com
+sudo docker rm -f peer2.iapeer.com
+sudo docker rm -f peer3.iapeer.com
 sudo docker rm -f cli
-
+###############################################
 sudo rm -rf ca/*
 sudo rm -rf tlsca/*
 sudo rm -rf admincerts/*
 sudo rm -rf channel-artifacts/*
-
+###############################################
 sudo rm -rf orderers/iaorderer/msp/admincerts/*
 sudo rm -rf orderers/iaorderer/msp/cacerts/*
 sudo rm -rf orderers/iaorderer/msp/tlscacerts/*
+###############################################
 sudo rm -rf orderers/iaorderer/orderer1/msp/admincerts/*
 sudo rm -rf orderers/iaorderer/orderer1/msp/cacerts/*
 sudo rm -rf orderers/iaorderer/orderer1/msp/keystore/*
@@ -32,9 +39,50 @@ sudo rm -rf orderers/iaorderer/orderer1/tls/user/*
 sudo rm -rf orderers/iaorderer/orderer1/tls/IssuerPublicKey
 sudo rm -rf orderers/iaorderer/orderer1/tls/IssuerRevocationPublicKey
 
+sudo rm -rf orderers/iaorderer/orderer2/msp/admincerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/keystore/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer2/msp/IssuerRevocationPublicKey
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/admincerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/keystore/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/user/*
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer2/msp/user/admin/IssuerRevocationPublicKey
+sudo rm -rf orderers/iaorderer/orderer2/tls/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer2/tls/keystore/*
+sudo rm -rf orderers/iaorderer/orderer2/tls/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer2/tls/user/*
+sudo rm -rf orderers/iaorderer/orderer2/tls/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer2/tls/IssuerRevocationPublicKey
+
+sudo rm -rf orderers/iaorderer/orderer3/msp/admincerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/keystore/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer3/msp/IssuerRevocationPublicKey
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/admincerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/keystore/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/user/*
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer3/msp/user/admin/IssuerRevocationPublicKey
+sudo rm -rf orderers/iaorderer/orderer3/tls/cacerts/*
+sudo rm -rf orderers/iaorderer/orderer3/tls/keystore/*
+sudo rm -rf orderers/iaorderer/orderer3/tls/signcerts/*
+sudo rm -rf orderers/iaorderer/orderer3/tls/user/*
+sudo rm -rf orderers/iaorderer/orderer3/tls/IssuerPublicKey
+sudo rm -rf orderers/iaorderer/orderer3/tls/IssuerRevocationPublicKey
+###############################################
 sudo rm -rf peers/iapeer/msp/admincerts/*
 sudo rm -rf peers/iapeer/msp/cacerts/*
 sudo rm -rf peers/iapeer/msp/tlscacerts/*
+###############################################
 sudo rm -rf peers/iapeer/peer1/msp/admincerts/*
 sudo rm -rf peers/iapeer/peer1/msp/cacerts/*
 sudo rm -rf peers/iapeer/peer1/msp/keystore/*
@@ -55,16 +103,98 @@ sudo rm -rf peers/iapeer/peer1/tls/user/*
 sudo rm -rf peers/iapeer/peer1/tls/IssuerPublicKey
 sudo rm -rf peers/iapeer/peer1/tls/IssuerRevocationPublicKey
 
+sudo rm -rf peers/iapeer/peer2/msp/admincerts/*
+sudo rm -rf peers/iapeer/peer2/msp/cacerts/*
+sudo rm -rf peers/iapeer/peer2/msp/keystore/*
+sudo rm -rf peers/iapeer/peer2/msp/signcerts/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/admincerts/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/cacerts/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/keystore/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/signcerts/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/user/*
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer2/msp/user/admin/IssuerRevocationPublicKey
+sudo rm -rf peers/iapeer/peer2/msp/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer2/msp/IssuerRevocationPublicKey
+sudo rm -rf peers/iapeer/peer2/tls/cacerts/*
+sudo rm -rf peers/iapeer/peer2/tls/keystore/*
+sudo rm -rf peers/iapeer/peer2/tls/signcerts/*
+sudo rm -rf peers/iapeer/peer2/tls/user/*
+sudo rm -rf peers/iapeer/peer2/tls/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer2/tls/IssuerRevocationPublicKey
+
+sudo rm -rf peers/iapeer/peer3/msp/admincerts/*
+sudo rm -rf peers/iapeer/peer3/msp/cacerts/*
+sudo rm -rf peers/iapeer/peer3/msp/keystore/*
+sudo rm -rf peers/iapeer/peer3/msp/signcerts/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/admincerts/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/cacerts/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/keystore/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/signcerts/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/user/*
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer3/msp/user/admin/IssuerRevocationPublicKey
+sudo rm -rf peers/iapeer/peer3/msp/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer3/msp/IssuerRevocationPublicKey
+sudo rm -rf peers/iapeer/peer3/tls/cacerts/*
+sudo rm -rf peers/iapeer/peer3/tls/keystore/*
+sudo rm -rf peers/iapeer/peer3/tls/signcerts/*
+sudo rm -rf peers/iapeer/peer3/tls/user/*
+sudo rm -rf peers/iapeer/peer3/tls/IssuerPublicKey
+sudo rm -rf peers/iapeer/peer3/tls/IssuerRevocationPublicKey
+###############################################
 sudo rm -rf rootca-admin/msp/cacerts/*
 sudo rm -rf rootca-admin/msp/keystore/*
 sudo rm -rf rootca-admin/msp/signcerts/*
 sudo rm -rf rootca-admin/msp/user/*
 sudo rm -rf rootca-admin/msp/IssuerPublicKey
 sudo rm -rf rootca-admin/msp/IssuerRevocationPublicKey
-
+###############################################
+sudo rm -rf rootca/msp/cacerts/*
+sudo rm -rf rootca/msp/keystore/*
+sudo rm -rf rootca/msp/signcerts/*
+sudo rm -rf rootca/msp/user/*
+sudo rm -rf rootca/msp/IssuerPublicKey
+sudo rm -rf rootca/msp/IssuerRevocationPublicKey
+###############################################
+sudo rm -rf roottlsca/msp/cacerts/*
+sudo rm -rf roottlsca/msp/keystore/*
+sudo rm -rf roottlsca/msp/signcerts/*
+sudo rm -rf roottlsca/msp/user/*
+sudo rm -rf roottlsca/msp/IssuerPublicKey
+sudo rm -rf roottlsca/msp/IssuerRevocationPublicKey
+###############################################
 sudo rm -rf roottlsca-admin/msp/cacerts/*
 sudo rm -rf roottlsca-admin/msp/keystore/*
 sudo rm -rf roottlsca-admin/msp/signcerts/*
 sudo rm -rf roottlsca-admin/msp/user/*
 sudo rm -rf roottlsca-admin/msp/IssuerPublicKey
 sudo rm -rf roottlsca-admin/msp/IssuerRevocationPublicKey
+###############################################
+sudo rm -rf rootca-admin-orderer/msp/cacerts/*
+sudo rm -rf rootca-admin-orderer/msp/keystore/*
+sudo rm -rf rootca-admin-orderer/msp/signcerts/*
+sudo rm -rf rootca-admin-orderer/msp/user/*
+sudo rm -rf rootca-admin-orderer/msp/IssuerPublicKey
+sudo rm -rf rootca-admin-orderer/msp/IssuerRevocationPublicKey
+###############################################
+sudo rm -rf roottlsca-admin-orderer/msp/cacerts/*
+sudo rm -rf roottlsca-admin-orderer/msp/keystore/*
+sudo rm -rf roottlsca-admin-orderer/msp/signcerts/*
+sudo rm -rf roottlsca-admin-orderer/msp/user/*
+sudo rm -rf roottlsca-admin-orderer/msp/IssuerPublicKey
+sudo rm -rf roottlsca-admin-orderer/msp/IssuerRevocationPublicKey
+###############################################
+sudo rm -rf rootca-admin-peer/msp/cacerts/*
+sudo rm -rf rootca-admin-peer/msp/keystore/*
+sudo rm -rf rootca-admin-peer/msp/signcerts/*
+sudo rm -rf rootca-admin-peer/msp/user/*
+sudo rm -rf rootca-admin-peer/msp/IssuerPublicKey
+sudo rm -rf rootca-admin-peer/msp/IssuerRevocationPublicKey
+###############################################
+sudo rm -rf roottlsca-admin-peer/msp/cacerts/*
+sudo rm -rf roottlsca-admin-peer/msp/keystore/*
+sudo rm -rf roottlsca-admin-peer/msp/signcerts/*
+sudo rm -rf roottlsca-admin-peer/msp/user/*
+sudo rm -rf roottlsca-admin-peer/msp/IssuerPublicKey
+sudo rm -rf roottlsca-admin-peer/msp/IssuerRevocationPublicKey
